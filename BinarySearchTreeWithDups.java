805e23d17a9354728eae0e1489af9a0782f4b1e8
sanand1695 <53537233+sanand1695@users.noreply.github.com>
2019-12-15 18:28:35 -0800

    Part A is working

--- BinarySearchTreeWithDups.java
+++ BinarySearchTreeWithDups.java
@@ -81,7 +81,7 @@ implements SearchTreeInterface<T>, java.io.Serializable {
 			if (comparison == 0) {
 				count++;
 			}
-			if (comparison < 0) {
+			if (comparison <= 0) {
 				currentNode = currentNode.getLeftChild();
 
 			} else { // if comparison > 0
@@ -99,65 +99,90 @@ implements SearchTreeInterface<T>, java.io.Serializable {
 	// MAKE SURE TO TAKE ADVANTAGE OF THE SORTED NATURE OF THE BST!
 	// Part A - Question 3
 	public int countGreaterRecursive(T target) {
-		return 0;
-	}
-	
-
-	// YOUR CODE HERE! MUST USE A STACK!! MUST NOT BE RECURSIVE!
-	// MAKE SURE TO TAKE ADVANTAGE OF THE SORTED NATURE OF THE BST!
-	// Part A - Question 4
-	public int countGreaterWithStack(T target) {
+		// this initial code is meant as a suggestion to get your started- use it or
+		// delete it!
 		int count = 0;
 		BinaryNode<T> rootNode = getRootNode();
-		Stack<BinaryNode<T>> nodeStack = new Stack<BinaryNode<T>>();
-		nodeStack.push(rootNode);
 
-		// consider a loop based on the stack!
-		while (!nodeStack.isEmpty()) {
-			BinaryNode<T> currentNode = nodeStack.pop();
-			if (currentNode.getData().compareTo(target) > 0) {
-				count++;
-				if (currentNode.hasLeftChild()) {
-					nodeStack.push(currentNode.getLeftChild());
-				}
-			}
-			if (currentNode.hasRightChild()) {
-				nodeStack.push(currentNode.getRightChild());
-			}
-		}
-		return count;
+		// consider a helper method!
+	
+	return countGreaterRecursiveHelper(target, rootNode);
 	}
-
-	// YOUR EXTRA CREDIT CODE HERE! THIS METHOD MUST BE O(n).
-	// YOU ARE ALLOWED TO USE A HELPER METHOD. THE METHOD CAN BE ITERATIVE OR
-	// RECURSIVE.
-	public int countUniqueValues() {
-		BinaryNode<T> maxNode = getRootNode();
-		while (maxNode.hasRightChild()) {
-			maxNode = maxNode.getRightChild();
+	
+	public int countGreaterRecursiveHelper(T target, BinaryNode<T> currentNode) {
+	
+		int result = 0;
+		
+		if(currentNode != null) {
+		
+		int comparison = currentNode.getData().compareTo(target);
+		
+		if(comparison <= 0) {
+			result = countGreaterRecursiveHelper(target, currentNode.getRightChild()) ;
+		} else {
+			result = 1 + countGreaterRecursiveHelper(target, currentNode.getLeftChild()) + 
+					countGreaterRecursiveHelper(target, currentNode.getRightChild()) ;
 		}
-		//+1 needed because this search will not count the highest value in the tree
-		return countUniqueValuesHelper(getRootNode(), maxNode.getData()) + 1;
+		}
+		return result;
 		
 	}
 	
-	private int countUniqueValuesHelper(BinaryNode<T> currentNode, T maxData) {
-		int total = 0;
-		
-		if (currentNode.hasLeftChild()) {
-			total += countUniqueValuesHelper(currentNode.getLeftChild(), currentNode.getData());
+	// YOUR CODE HERE! MUST USE A STACK!! MUST NOT BE RECURSIVE!
+		// MAKE SURE TO TAKE ADVANTAGE OF THE SORTED NATURE OF THE BST!
+		// Part A - Question 4
+		public int countGreaterWithStack(T target) {
+			int count = 0;
+			BinaryNode<T> rootNode = getRootNode();
+			Stack<BinaryNode<T>> nodeStack = new Stack<BinaryNode<T>>();
+			nodeStack.push(rootNode);
+
+			// consider a loop based on the stack!
+			while (!nodeStack.isEmpty()) {
+				BinaryNode<T> currentNode = nodeStack.pop();
+				if (currentNode.getData().compareTo(target) > 0) {
+					count++;
+					if (currentNode.hasLeftChild()) {
+						nodeStack.push(currentNode.getLeftChild());
+					}
+				}
+				if (currentNode.hasRightChild()) {
+					nodeStack.push(currentNode.getRightChild());
+				}
+			}
+			return count;
+		}
+
+		// YOUR EXTRA CREDIT CODE HERE! THIS METHOD MUST BE O(n).
+		// YOU ARE ALLOWED TO USE A HELPER METHOD. THE METHOD CAN BE ITERATIVE OR
+		// RECURSIVE.
+		public int countUniqueValues() {
+			BinaryNode<T> maxNode = getRootNode();
+			while (maxNode.hasRightChild()) {
+				maxNode = maxNode.getRightChild();
+			}
+			//+1 needed because this search will not count the highest value in the tree
+			return countUniqueValuesHelper(getRootNode(), maxNode.getData()) + 1;
+			
 		}
 		
-		if (currentNode.getData().compareTo(maxData) < 0) {
-			total++;
+		private int countUniqueValuesHelper(BinaryNode<T> currentNode, T maxData) {
+			int total = 0;
+			
+			if (currentNode.hasLeftChild()) {
+				total += countUniqueValuesHelper(currentNode.getLeftChild(), currentNode.getData());
+			}
+			
+			if (currentNode.getData().compareTo(maxData) < 0) {
+				total++;
+			}
+			
+			if (currentNode.hasRightChild()) {
+				total += countUniqueValuesHelper(currentNode.getRightChild(), maxData);
+			}
+			
+			return total;
 		}
-		
-		if (currentNode.hasRightChild()) {
-			total += countUniqueValuesHelper(currentNode.getRightChild(), maxData);
-		}
-		
-		return total;
-	}
 
 	public int getLeftHeight() {
 		BinaryNode<T> rootNode = getRootNode();
